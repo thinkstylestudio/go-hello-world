0a1f89dd581911e5d96b9d5332c9242004d8ade5
Theron Smith <theron@thinkstylestudio.com>
2023-10-25 13:22:17 -0700

    add two different output examples from what I can recall the specs asked for

--- main.go
+++ main.go
@@ -1,71 +1,74 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"helloworld/internal/routes"
-	"log"
-	"net/http"
 )
 
-type MyData struct {
-	ID        int
-	Migration string
-	Batch     int
+// create a struct to represent each of the timeseries threshold pairs
+type TimeSeriesThreshold struct {
+	PointInTime float64
+	Threshold   float64
+}
+
+// given the collection/array/slice of structs (pairs) and a given threshold return a
+func processThreshold(pairs []TimeSeriesThreshold, threshold float64) []float64 {
+	var output []float64
+	// go through and append to output timestamps that are above the threshold
+	for _, pair := range pairs {
+		if pair.Threshold >= threshold {
+			output = append(output, pair.PointInTime)
+		}
+	}
+	// assuming out of [10, 30]
+	return output
+}
+
+func processStartEndPairings(pairs []TimeSeriesThreshold, threshold float64) []float64 {
+	var output []float64
+
+	aboveThreshold := false
+
+	// go through and figure the beginning threshold and ending threshold pairs
+	for index, pair := range pairs {
+
+		if !aboveThreshold && pair.Threshold >= threshold {
+			output = append(output, pair.PointInTime)
+			aboveThreshold = true
+		}
+		if aboveThreshold && pair.Threshold < threshold {
+			output = append(output, pairs[index-1].PointInTime)
+			aboveThreshold = false
+		}
+
+	}
+
+	if aboveThreshold {
+		output = append(output, pairs[len(pairs)-1].PointInTime)
+	}
+
+	// assuming out of [[10, 10],[30,40]]
+	return output
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/trials-database")
 
-	if err != nil {
-		log.Fatal(err)
+	// Input PointInTime: [[10,0.9],[20,0.1],[30,0.8], [40, 0.9]]
+	// Input 0.8
+	// Output [[10, 30]]
+
+	// Create a slice
+	pairs := []TimeSeriesThreshold{
+		{PointInTime: 10, Threshold: 0.9},
+		{PointInTime: 20, Threshold: 0.1},
+		{PointInTime: 30, Threshold: 0.8},
+		{PointInTime: 40, Threshold: 0.9},
 	}
-	defer db.Close()
+	threshold := 0.8
 
-	rows, err := db.Query("SELECT id, migration, batch FROM migrations")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+	startEndingResult := processStartEndPairings(pairs, threshold)
+	fmt.Println(startEndingResult)
 
-	var data []MyData
+	thresholdResult := processThreshold(pairs, threshold)
+	fmt.Println(thresholdResult)
 
-	for rows.Next() {
-		var d MyData
-		err := rows.Scan(&d.ID, &d.Migration, &d.Batch)
-		if err != nil {
-			log.Fatal(err)
-		}
-		data = append(data, d)
-	}
-
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(data)
-
-	fmt.Printf("This output\n")
-
-	fmt.Print("--------------\n")
-
-	newServer(data)
-
-}
-
-func newServer(data []MyData) {
-
-	router := routes.NewRouter(data)
-
-	port := 8080
-	address := fmt.Sprintf(":%d", port)
-	fmt.Printf("Server is listening on http://localhost%s\n", address)
-	err := http.ListenAndServe(
-		address,
-		router,
-	)
-	if err != nil {
-		panic(err)
-	}
 }
