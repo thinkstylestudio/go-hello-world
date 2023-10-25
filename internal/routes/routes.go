83771a40d2a76c6ef4f3b8e0f28987a55468968d
Theron Smith <theron@thinkstylestudio.com>
2023-10-25 11:44:23 -0700

    playing with stuff

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,20 +2,26 @@ package routes
 
 import (
 	"fmt"
+	"helloworld"
 	"net/http"
 )
 
-func NewRouter() http.Handler {
+var apiData string
+
+func NewRouter(data []main.MyData) http.Handler {
+	apiData = data
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", apiDataHandler)
 
 	return mux
 }
 
+//const apiData = "some api Data"
+
 func apiDataHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	data := "some api Data"
-	_, err := fmt.Fprintln(responseWriter, data)
+	_, err := fmt.Fprintln(responseWriter, apiData)
 	if err != nil {
-		panic(err)
+		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
--- main.go
+++ main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"helloworld/internal/routes"
-
+	"log"
 	"net/http"
 )
 
+type MyData struct {
+	ID        int
+	Migration string
+	Batch     int
+}
+
 func main() {
-	router := routes.NewRouter()
-	fmt.Printf("This output")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/trials-database")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, migration, batch FROM migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var data []MyData
+
+	for rows.Next() {
+		var d MyData
+		err := rows.Scan(&d.ID, &d.Migration, &d.Batch)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, d)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(data)
+
+	fmt.Printf("This output\n")
 
 	fmt.Print("--------------\n")
+
+	newServer(data)
+
+}
+
+func newServer(data []MyData) {
+
+	router := routes.NewRouter(data)
 
 	port := 8080
 	address := fmt.Sprintf(":%d", port)
@@ -23,5 +68,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 }
